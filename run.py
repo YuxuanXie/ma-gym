8696fc218f736a55d0e3a6a3c664bbc3166a71e7
Yuxuan <yuxuan.xie@hotmail.com>
2021-02-03 21:52:00 +0800

    Yx: add qmix, still debugging

---
+++ run.py
@@ -0,0 +1,234 @@
+import gym
+import ma_gym 
+import random
+import datetime
+import numpy as np
+import tensorflow as tf
+
+
+class QMix():
+    def __init__(self, env, num_s, num_a, lr=0.0001, gamma=0.99, replace_target_iter=2000,
+                 memory_size=2000000, batch_size=256*50, epsilon=1, epsilon_decay=0.0001):
+        self.env = env
+        self.name = "qmix"
+        self.num_global_s = num_s
+        self.num_s = num_s / 2
+        self.num_a = num_a
+        self.lr = lr
+        self.gamma = gamma
+        self.replace_target_iter = replace_target_iter
+        self.memory_size = memory_size
+        self.batch_size = batch_size
+        self.epsilon = epsilon
+        self.epsilon_decay = epsilon_decay
+        self.epsilon_min = 0.1
+        self.learn_step_cnt = 0  # total learning step
+        self.episode_cnt = 0
+        self.memory = []
+        self.memory_counter = 0
+        self._build_net()
+
+        t_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=self.name + '/target_net')
+        e_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=self.name + '/eval_net')
+
+        with tf.variable_scope('soft_replacement'):
+            self.target_replace_op = [tf.assign(t, e) for t, e in zip(t_params, e_params)]
+
+        self.sess = tf.Session()
+        self.sess.run(tf.global_variables_initializer())
+
+        current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+        train_log_dir = 'logs/' + current_time
+        self.summary_writer = tf.summary.FileWriter(train_log_dir, self.sess.graph)
+
+
+    def _build_net(self):
+        with tf.variable_scope(self.name):
+            # ------------------ all inputs ------------------------
+            self.S = tf.placeholder(tf.float32, [None, self.num_global_s], name='S')  # input Global State
+            self.s = tf.placeholder(tf.float32, [None, self.num_s], name='s1')  # input state for agent1
+            self.S_ = tf.placeholder(tf.float32, [None, self.num_global_s], name='S_')  # input Next Global State
+            self.s_ = tf.placeholder(tf.float32, [None, self.num_s], name='s1_')  # input next state for agent1
+            self.R = tf.placeholder(tf.float32, [None, ], name='R')  # input Reward
+            self.a1 = tf.placeholder(tf.int32, [None, ], name='a1')  # input Action for agent1
+            self.a2 = tf.placeholder(tf.int32, [None, ], name='a2')  # input Action for agent2
+            self.done = tf.placeholder(tf.float32, [None, ], name='done')  # input Done info ???
+
+            self.q1_a1 = tf.placeholder(tf.float32, [None, ], name='q1_eval_value_wrt_a1')
+            self.q2_a2 = tf.placeholder(tf.float32, [None, ], name='q2_eval_value_wrt_a2')
+            self.q1_m_ = tf.placeholder(tf.float32, [None, ], name='q1_value_next_max')
+            self.q2_m_ = tf.placeholder(tf.float32, [None, ], name='q2_value_next_max')
+
+            w_initializer, b_initializer = tf.random_normal_initializer(0., 0.1), tf.constant_initializer(0.0)
+
+            # ------------------ build evaluate_net ------------------
+            with tf.variable_scope('eval_net'):
+                a_fc1 = tf.layers.dense(self.s, 128, tf.nn.relu, kernel_initializer=w_initializer,
+                                        bias_initializer=b_initializer, name='agent_fc1_e')
+                # a_fc2 = tf.layers.dense(a_fc1, 128, tf.nn.relu, kernel_initializer=w_initializer,
+                #                         bias_initializer=b_initializer, name='agent_fc2_e')
+                # a_fc3 = tf.layers.dense(a_fc2, 64, tf.nn.relu, kernel_initializer=w_initializer,
+                #                         bias_initializer=b_initializer, name='agent_fc3_e')
+                self.q_eval = tf.layers.dense(a_fc1, self.num_a, kernel_initializer=w_initializer,
+                                              bias_initializer=b_initializer, name='q_e')
+
+            # ------------------ build target_net ------------------
+            with tf.variable_scope('target_net'):
+                a_fc1_ = tf.layers.dense(self.s_, 128, tf.nn.relu, kernel_initializer=w_initializer,
+                                         bias_initializer=b_initializer, name='agent_fc1_t')
+                # a_fc2_ = tf.layers.dense(a_fc1_, 128, tf.nn.relu, kernel_initializer=w_initializer,
+                #                          bias_initializer=b_initializer, name='agent_fc2_t')
+                # a_fc3_ = tf.layers.dense(a_fc2_, 64, tf.nn.relu, kernel_initializer=w_initializer,
+                #                          bias_initializer=b_initializer, name='agent_fc3_t')
+                self.q_next = tf.layers.dense(a_fc1_, self.num_a, kernel_initializer=w_initializer,
+                                              bias_initializer=b_initializer, name='q_t')
+
+            # ------------------ build mixing_net ------------------
+            with tf.variable_scope('mixing_net'):
+                # a1_indices = tf.stack([tf.range(tf.shape(self.a1)[0], dtype=tf.int32), self.a1], axis=1, name='a1_indices')
+                # a2_indices = tf.stack([tf.range(tf.shape(self.a2)[0], dtype=tf.int32), self.a2], axis=1, name='a2_indices')
+                # a3_indices = tf.stack([tf.range(tf.shape(self.a3)[0], dtype=tf.int32), self.a3], axis=1, name='a3_indices')
+                # q1_a = tf.gather_nd(params=self.q1_eval, indices=a1_indices, name='q1_eval_wrt_a')
+                # q2_a = tf.gather_nd(params=self.q2_eval, indices=a2_indices, name='q2_eval_wrt_a')
+                # q3_a = tf.gather_nd(params=self.q3_eval, indices=a3_indices, name='q3_eval_wrt_a')
+                self.q_concat = tf.stack([self.q1_a1, self.q2_a2], axis=1, name='q_concat')
+                self.q_concat_ = tf.stack([self.q1_m_, self.q2_m_], axis=1, name='q_concat_next')  # todo
+
+                # with tf.variable_scope('eval_net'):
+                with tf.variable_scope('hyper_layer1'):
+                    non_abs_w1 = tf.layers.dense(inputs=self.S, units=2*32, kernel_initializer=w_initializer,
+                                                 bias_initializer=b_initializer, name='non_abs_w1')
+                    self.w1 = tf.reshape(tf.abs(non_abs_w1), shape=[-1, 2, 32], name='w1')
+                    self.b1 = tf.layers.dense(inputs=self.S, units=32, kernel_initializer=w_initializer,
+                                              bias_initializer=b_initializer, name='non_abs_b1')
+                with tf.variable_scope('hyper_layer2'):
+                    non_abs_w2 = tf.layers.dense(inputs=self.S, units=32 * 1, kernel_initializer=w_initializer,
+                                                 bias_initializer=b_initializer, name='non_abs_w2')
+                    self.w2 = tf.reshape(tf.abs(non_abs_w2), shape=[-1, 32, 1], name='w2')
+                    bef_b2 = tf.layers.dense(inputs=self.S, units=32, activation=tf.nn.relu,
+                                             kernel_initializer=w_initializer, bias_initializer=b_initializer, name='bef_b2')
+                    self.b2 = tf.layers.dense(inputs=bef_b2, units=1, kernel_initializer=w_initializer,
+                                              bias_initializer=b_initializer, name='non_abs_b2')
+
+                with tf.variable_scope('layer_mix_eval'):
+                    lin1 = tf.matmul(tf.reshape(self.q_concat, shape=[-1, 1, 2]), self.w1) + tf.reshape(self.b1, shape=[-1, 1, 32])
+                    a1 = tf.nn.elu(lin1, name='a1')
+                    self.Q_tot = tf.reshape(tf.matmul(a1, self.w2), shape=[-1, 1]) + self.b2
+
+                with tf.variable_scope('layer_mix_target'):
+                    lin1_ = tf.matmul(tf.reshape(self.q_concat_, shape=[-1, 1, 2]), self.w1) + tf.reshape(self.b1, shape=[-1, 1, 32])
+                    a1_ = tf.nn.elu(lin1_, name='a1_')
+                    self.Q_tot_ = tf.reshape(tf.matmul(a1_, self.w2), shape=[-1, 1]) + self.b2
+
+            # todo: add q_target, loss, train_op
+            with tf.variable_scope('q_target'):
+                q_target = self.R + (1 - self.done) * self.gamma * self.Q_tot_
+                self.q_target = tf.stop_gradient(q_target)
+            with tf.variable_scope('loss'):
+                self.loss = tf.reduce_mean(tf.squared_difference(self.q_target, self.Q_tot, name='TD_error'))
+            with tf.variable_scope('train'):
+                self._train_op = tf.train.AdamOptimizer(self.lr).minimize(self.loss)
+             
+
+
+    def act(self, state):
+        if np.random.uniform() > self.epsilon :# pick the argmax action
+            s = np.array(state)
+            if len(s.shape) < 2:
+                s = np.array(state)[np.newaxis, :]
+            q_eval = self.sess.run(self.q_eval, feed_dict={self.s: s})
+            action = np.argmax(q_eval, axis=-1).tolist()
+        else:  # pick random action
+            action = self.env.action_space.sample() 
+        return action
+
+    def store(self, EXP):
+        self.memory_counter += 1
+        if len(self.memory) > self.memory_size:
+            # random replacement
+            index = np.random.randint(0, self.memory_size)
+            self.memory[index] = EXP
+        else:
+            self.memory.append(EXP)
+
+    def learn(self):
+        if len(self.memory) < self.batch_size :
+            return
+        # sample batch exp from memory
+        if self.learn_step_cnt % 10000 == 0:
+            print(self.name, 'update ----> learn_step_cnt', self.learn_step_cnt)
+        batch_exp = random.sample(self.memory, self.batch_size)
+        S, s1, s2, a1, a2, R, S_, s1_, s2_, done = [[] for _ in range(10)]
+        for exp in batch_exp:
+            S.append(exp[0])
+            s1.append(exp[1])
+            s2.append(exp[2])
+            a1.append(exp[3])
+            a2.append(exp[4])
+            R.append(exp[5])
+            S_.append(exp[6])
+            s1_.append(exp[7])
+            s2_.append(exp[8])
+            done.append(exp[9])
+        # to get q_tot
+        q1_eval = self.sess.run(self.q_eval, feed_dict={self.s: s1})
+        q2_eval = self.sess.run(self.q_eval, feed_dict={self.s: s2})
+        row_index = [_ for _ in range(self.batch_size)]
+        q1_a1 = q1_eval[row_index, a1]
+        q2_a2 = q2_eval[row_index, a2]
+
+        # to get q_tot_
+        q1_ = self.sess.run(self.q_next, feed_dict={self.s_: s1_})
+        q2_ = self.sess.run(self.q_next, feed_dict={self.s_: s2_})
+        q1_m_ = np.max(q1_, axis=1)
+        q2_m_ = np.max(q2_, axis=1)
+        q_tot_ = self.sess.run(self.Q_tot_,
+                               feed_dict={self.S: S_, self.q1_m_: q1_m_, self.q2_m_: q2_m_})
+
+        # update
+        _, cost = self.sess.run([self._train_op, self.loss],
+                                feed_dict={self.S: S, self.q1_a1: q1_a1, self.q2_a2: q2_a2,
+                                           self.a1: a1, self.a2: a2,
+                                           self.R: R, self.Q_tot_: q_tot_, self.done: done})
+        # print('cost', cost)
+
+        self.write_summary_scalar('loss', cost, self.learn_step_cnt)
+        self.write_summary_scalar('epsilon', self.epsilon, self.learn_step_cnt)
+        self.write_summary_scalar('memory_cnt', self.memory_counter, self.learn_step_cnt)
+        self.epsilon = max(self.epsilon - self.epsilon_decay, self.epsilon_min)  # decay epsilon
+        self.learn_step_cnt += 1
+
+        # check to do the soft replacement of target net
+        if self.learn_step_cnt % self.replace_target_iter == 0 and self.learn_step_cnt:
+            self.sess.run(self.target_replace_op)
+
+
+    def train(self):
+        
+        for i in range(50000):        
+            done_n = [False for _ in range(env.n_agents)]
+            ep_reward = 0
+            obs = env.reset()
+            while not all(done_n):
+                # env.render()
+                action = self.act(obs)
+                obs_n, reward_n, done_n, info = env.step(action)
+                ep_reward += sum(reward_n)
+                obs_glob = [obs[0] + obs[1]]
+                obs_glob_next = [obs_n[0] + obs_n[1]]
+                # try:
+                #     print(obs_glob + obs + action + [sum(reward_n)] + obs_glob_next + obs_n + [all(done_n)])
+                # except:
+                #     import pdb; pdb.set_trace()
+                self.store(obs_glob + obs + action + [sum(reward_n)] + obs_glob_next + obs_n + [all(done_n)])
+                obs = obs_n
+                self.learn()
+            self.write_summary_scalar("ep_reward", ep_reward, self.learn_step_cnt)
+    def write_summary_scalar(self, tag, value, iteration):
+        self.summary_writer.add_summary(tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)]), iteration)
+
+
+env = gym.make('Switch2-v0')
+alg = QMix(env, 4, 5)
+
+alg.train()
